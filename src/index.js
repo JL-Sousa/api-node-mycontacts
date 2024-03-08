b92d488f0166694f3ba0f375fe2878370b014f7f
Jonyelson Lopes <jonyelson20@gmail.com>
2024-03-08 12:00:45 -0300

    feat: created api to manage contacts.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('express-async-errors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use((error, request, response, next) => {
+  response.sendStatus(500);
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`🔥 Server started at http://localhost:${port}`));
